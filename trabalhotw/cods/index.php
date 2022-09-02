b186fd31e5b06553f33365b76c485a05e1af581c
AnaLuiza06 <101757966+AnaLuiza06@users.noreply.github.com>
2022-09-02 00:02:43 -0300

    Add files via upload

---
+++ trabalhotw/cods/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Margaret Hamilton</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="../css/style-index.css">
+</head>
+<body>
+
+	<header>
+	</header>
+		<nav>
+			<h2>Margaret Hamilton</h2>
+			<ul>
+				<li><a href="#">Biografia</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contato</a></li>
+			</ul>
+			<button><a href="#">Entrar</a></button>
+		</nav>
+
+	<div class="header">
+		<h1>Mãe Cientista na Liderança do Apollo 11</h1>
+			
+		<div class="img-header">
+			<img src="http://horizontes.sbc.org.br/wp-content/uploads/2021/05/13007h.jpg">
+		</div>
+	</div>
+
+	<main>
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<h3>Biografia</h3>
+				<p></p>
+			</div>
+		</div>
+	</main>
+
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
